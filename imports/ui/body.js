32f86403656ba017e21197a64e793e56c5730967
Alex Channell <achannell@morristechnology.com>
2016-04-13 23:48:56 -0400

    graphing in real time

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,6 +4,7 @@ import { PyData } from '../api/pydata';
 
 import './body.html';
 
+
 Template.body.helpers({
    pydata() {
        return PyData.find({});
@@ -16,32 +17,22 @@ var line_chart;
 PyData.find().observeChanges({
     added: function (id, user) {
         console.log("Updating!");
-
-        line_chart.update();
-    }
-});
-
-
-function get_data() {
-    var pydata_list = [];
-    var pydata = PyData.find({});
-    pydata.forEach(function (pydata_instance) {
-        pydata_list.push(pydata_instance.data);
-    });
-    console.log(pydata_list);
-
-    return pydata_list;
-}
-
-Template.graph.rendered = function(){
-    var canvas = $("#chart");
+        // line_chart.update();
+        var canvas = $("#chart");
     var context = canvas[0].getContext('2d');
 
     console.log(get_data());
     // console.log(pydata);
-
+    var labels_list = [];
+    
+    for (var i = 0; i < 30; i++){
+        labels_list.push(i);
+    }
+        
+    
     var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        // labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: labels_list,
         datasets: [
             {
                 label: "My First dataset",
@@ -67,4 +58,65 @@ Template.graph.rendered = function(){
     };
 
     line_chart = new Chart(context).Line(data);
+    line_chart.update();
+    console.log(line_chart);
+    }
+});
+
+
+function get_data() {
+    var pydata_list = [];
+    var pydata = PyData.find({});
+    pydata.forEach(function (pydata_instance) {
+        pydata_list.push(pydata_instance.data);
+    });
+    console.log(pydata_list);
+
+    return pydata_list;
+}
+
+Template.graph.rendered = function(){
+    var canvas = $("#chart");
+    var context = canvas[0].getContext('2d');
+
+    console.log(get_data());
+    // console.log(pydata);
+    
+    var labels_list = [];
+    
+    for (var i = 0; i < 30; i++){
+        labels_list.push(i);
+    }
+        
+    
+    var data = {
+        // labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: labels_list,
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: get_data()
+            }
+            // {
+            //     label: "My Second dataset",
+            //     fillColor: "rgba(151,187,205,0.2)",
+            //     strokeColor: "rgba(151,187,205,1)",
+            //     pointColor: "rgba(151,187,205,1)",
+            //     pointStrokeColor: "#fff",
+            //     pointHighlightFill: "#fff",
+            //     pointHighlightStroke: "rgba(151,187,205,1)",
+            //     data: [28, 48, 40, 19, 86, 27, 90]
+            // }
+        ]
+    };
+
+    line_chart = new Chart(context).Line(data);
+    line_chart.update();
+    console.log(line_chart);
 };
