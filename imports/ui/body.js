b15af343b88473107213f6f57ea217f1a1781c9f
Alex Channell <achannell@morristechnology.com>
2016-04-16 19:54:11 -0400

    cleanup

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -44,16 +44,6 @@ PyData.find().observeChanges({
                 pointHighlightStroke: "rgba(220,220,220,1)",
                 data: get_data()
             }
-            // {
-            //     label: "My Second dataset",
-            //     fillColor: "rgba(151,187,205,0.2)",
-            //     strokeColor: "rgba(151,187,205,1)",
-            //     pointColor: "rgba(151,187,205,1)",
-            //     pointStrokeColor: "#fff",
-            //     pointHighlightFill: "#fff",
-            //     pointHighlightStroke: "rgba(151,187,205,1)",
-            //     data: [28, 48, 40, 19, 86, 27, 90]
-            // }
         ]
     };
 
@@ -103,16 +93,6 @@ Template.graph.rendered = function(){
                 pointHighlightStroke: "rgba(220,220,220,1)",
                 data: get_data()
             }
-            // {
-            //     label: "My Second dataset",
-            //     fillColor: "rgba(151,187,205,0.2)",
-            //     strokeColor: "rgba(151,187,205,1)",
-            //     pointColor: "rgba(151,187,205,1)",
-            //     pointStrokeColor: "#fff",
-            //     pointHighlightFill: "#fff",
-            //     pointHighlightStroke: "rgba(151,187,205,1)",
-            //     data: [28, 48, 40, 19, 86, 27, 90]
-            // }
         ]
     };
 
